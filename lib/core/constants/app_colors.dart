db0cb34d7e7a5438cafc567ede381e1a6692b017
Raffaela Castro <raffacst@outlook.com>
2025-02-05 21:15:24 -0300

    chore: :lipstick: add color palette and text styles constants
    add color palette and text styles constants
    
    add color palette and text styles constants

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color redPrimary = Color(0xFFD11621);
+  static const Color bgModalMain = Color(0xFFD9D9D9);
+  static const Color bgModalSecondary = Color(0xFFF6F6F6);
+  static const Color bgCards = Color(0xFFFFFFFF);
+  static const Color btnReserve = Color(0xFF1ABA8E);
+  static const Color progressInactive = Color(0xFFBFBFBF);
+  static const Color progressActive = Color(0xFF959595);
+  static const Color iconScore = Color(0xFFFFB200);
+
+  // Font colors
+  static const Color fontPrimary = Color(0xFF373737);
+  static const Color fontSecondary = Color(0xFF464646);
+  static const Color fontTertiary = Color(0xFF959595);
+  static const Color fontDark = Color(0xFF090A0A);
+
+  // Favorite icon colors
+  static const Color favUnselected = Color(0xFFBFBFBF);
+  static const Color favSelected = Color(0xFFD11621);
+}
---
+++ lib/core/constants/app_text_styles.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTextStyles {
+  static final TextStyle os12Reg = GoogleFonts.openSans(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+  );
+
+  static final TextStyle os12SemiBold = GoogleFonts.openSans(
+    fontSize: 12,
+    fontWeight: FontWeight.w600,
+  );
+
+  static final TextStyle os16SemiBold = GoogleFonts.openSans(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+  );
+
+  static final TextStyle os24Reg = GoogleFonts.openSans(
+    fontSize: 24,
+    fontWeight: FontWeight.w400,
+  );
+
+  static final TextStyle os16Reg = GoogleFonts.openSans(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+  );
+
+  static final TextStyle os10SemiBold = GoogleFonts.openSans(
+    fontSize: 10,
+    fontWeight: FontWeight.w600,
+  );
+}
